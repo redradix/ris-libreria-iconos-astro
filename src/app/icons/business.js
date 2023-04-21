f27b77873a72557c9e7a3f7b807560288dac9c11
Sonia Ruiz <yune.vk@gmail.com>
2023-04-21 14:22:35 +0200

    🩹 Order sizes

--- src/app/icons/business.js
+++ src/app/icons/business.js
@@ -1,42 +1,60 @@
 export const formatIcons = (icons) => {
   const formattedIcons = icons.map(({ name, svgData }) => {
-    const [displayName, tag, size] = name.split(" | ");
+    const [displayName, tag, size] = name.split(" | ")
 
     const formattedIcon = {
       name: displayName,
       tag,
       size,
-      svgData: svgData.replace("<svg", `<svg aria-label="${displayName}"`)
-    };
-
-    return formattedIcon;
-  });
-
-  const groupedByName = formattedIcons.reduce((acc, icon) => {
-    const { name, size } = icon;
-
-    if (!acc[name]) {
-      acc[name] = {};
+      svgData: svgData.replace("<svg", `<svg aria-label="${displayName}"`),
     }
 
-    acc[name][size] = icon;
+    return formattedIcon
+  })
 
-    return acc;
-  }, {});
+  const groupedByName = formattedIcons.reduce((acc, icon) => {
+    const { name, size } = icon
 
-  return groupedByName;
-};
+    if (!acc[name]) {
+      acc[name] = {}
+    }
+
+    acc[name][size] = icon
+
+    return acc
+  }, {})
+
+  return groupedByName
+}
 
 export const getAvailableSizes = (groupedIcons) => {
-  const sizes = new Set();
+  const sizes = new Set()
 
   for (const iconName in groupedIcons) {
-    const icon = groupedIcons[iconName];
+    const icon = groupedIcons[iconName]
 
     for (const size in icon) {
-      sizes.add(size);
+      sizes.add(size)
     }
   }
 
-  return Array.from(sizes);
-};
+  const arrayOfSizes = Array.from(sizes)
+
+  const parseSize = (sizeStr) => {
+    const parts = sizeStr.split("-").map((s) => parseInt(s))
+    return parts.length === 1 ? [parts[0], parts[0]] : parts
+  }
+
+  const sizesAsTuples = arrayOfSizes.map(parseSize)
+  const sortedSizes = sizesAsTuples.sort((a, b) => a[0] - b[0] || a[1] - b[1])
+
+  const sizeTupleToString = (sizeTuple) => {
+    return sizeTuple[0] === sizeTuple[1]
+      ? `${sizeTuple[0]}px`
+      : `${sizeTuple[0]}-${sizeTuple[1]}px`
+  }
+
+  const sortedArr = sortedSizes.map(sizeTupleToString)
+
+  return sortedArr
+}
